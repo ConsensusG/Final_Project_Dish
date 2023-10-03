90352b95378c1960f9808879f2df976090f9fc99
Justin Wescott <YungCircus@gmail.com>
2023-10-03 16:21:33 -0400

    Working everything and a working unstyled favorites page

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,15 +2,40 @@ import { useState, useEffect } from 'react';
 import fetchRecipes from './utils/FetchRecipes';
 import './AppStyles.css'
 import Slider from "react-slick";
+import { BrowserRouter as Router,  Route,  Link,  Routes} from "react-router-dom";
 
 
 const App = () => {
   const [query, setQuery] = useState('');
   const [recipes, setRecipes] = useState<any[]>([]);
-  const [expanded, setExpanded] = useState<number | null>(null);
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
   const [showModalContent, setShowModalContent] = useState<boolean>(false);
   const [modalRecipe, setModalRecipe] = useState<any>(null);
+  const [favoriteRecipes, setFavoriteRecipes] = useState<any[]>([]);
+
+  function Favorites() {
+    return (
+      <div className="cards-container">
+        {favoriteRecipes.map((recipe: any, index: number) => (
+          <div className="card" style={{ width: "18rem" }} key={index}>
+            <img src={recipe.image} className="card-img-top" alt={recipe.title} />
+            <div className="card-body">
+              <h5 className="card-title">{recipe.title}</h5>
+              <p className="card-text">
+                <span dangerouslySetInnerHTML={{
+                  __html: recipe.summary.length > 100
+                    ? recipe.summary.substring(0, 100) + '...'
+                    : recipe.summary
+                }}></span>
+              </p>
+              <button className="btn btn-secondary view-details-btn" onClick={() => toggleModal(recipe)}>View Details</button>
+              <button className="btn btn-secondary add-to-faves-btn" onClick={() => addToFavorites(recipe)}>Add to Faves</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
 
   const handleSearch = async () => {
@@ -95,84 +120,117 @@ const App = () => {
     // ... any other settings you want
   };
 
+  const addToFavorites = (recipe: any) => {
+    // Check if the recipe is already in the favorites list
+    if (!favoriteRecipes.some(favRecipe => favRecipe.id === recipe.id)) {
+      setFavoriteRecipes(prevFavorites => [...prevFavorites, recipe]);
+    }
+  };
+
+  useEffect(() => {
+    console.log('Favorite Recipes:', favoriteRecipes);
+  }, [favoriteRecipes]);
+  
+  const deleteFavorite = (recipeToDelete: any) => {
+    setFavoriteRecipes(prevFavorites => 
+        prevFavorites.filter(recipe => recipe.id !== recipeToDelete.id)
+    );
+};
+
 
   return (
-    
-    <>
-      <div className="search-main-container">
-        <div className="search-container">
-          <input
-            className="search-input"
-            type="text"
-            value={query}
-            onChange={e => setQuery(e.target.value)}
-            placeholder="Search for recipes..."
-          />
-          <button className="search-button" onClick={handleSearch}>Search</button>
-        </div>
-        <div className="filter-icons">
-          <img
-            src="/icons/gluten-free.png"
-            alt="Gluten Free"
-            className="icon-button"
-            onClick={() => handleKeywordSearch('gluten free')}
-          />
-          <img
-            src="/icons/vegetarian.png"
-            alt="Vegetarian"
-            className="icon-button"
-            onClick={() => handleKeywordSearch('vegetarian')}
-          />
-          <img
-            src="/icons/vegan.png"
-            alt="Vegan"
-            className="icon-button"
-            onClick={() => handleKeywordSearch('vegan')}
-          />
-        </div>
-      </div>
-
-
-      <div className="cards-container">
-        <Slider {...sliderSettings}>
-          {recipes.map((recipe: any, index: number) => (
-            <div className="card" style={{ width: "18rem" }} key={index}>
-              <img src={recipe.image} className="card-img-top" alt={recipe.title} />
-              <div className="card-body">
-                <h5 className="card-title">{recipe.title}</h5>
-                <p className="card-text">
-                  <span dangerouslySetInnerHTML={{
-                    __html: recipe.summary.length > 100
-                      ? recipe.summary.substring(0, 100) + '...'
-                      : recipe.summary
-                  }}></span>
-                </p>
-                <button className="btn btn-secondary view-details-btn" onClick={() => toggleModal(recipe)}>View Details</button>
-                <a href="#" className="btn btn-secondary add-to-faves-btn">Add to Faves</a>
-
-              </div>
+    <Router>
+        <nav className="navbar navbar-dark bg-dark fixed-top">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    <img src="/icons/icons8-meal-48.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top" />
+                    DISH
+                </Link>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to="/favorites" className="nav-link">Favorites</Link>
+                    </li>
+                </ul>
             </div>
-          ))}
-        </Slider>
-</div>
-{isModalOpen && (
-  <div className="modal">
-    <div className={`modal-content ${showModalContent ? 'show' : ''}`}>
-      <span className="close-button" onClick={() => { setModalOpen(false); setShowModalContent(false); }}>×</span>
+        </nav>
 
-      {modalRecipe && (
-        <>
-          <h5 className="card-title">{modalRecipe.title}</h5>
-          <img src={modalRecipe.image} className="card-img-top" alt={modalRecipe.title} />
-          <p dangerouslySetInnerHTML={{ __html: modalRecipe.summary }}></p>
-        </>
-      )}
-    </div>
-  </div>
-)}
-
-</>
+        <Routes>
+            <Route path="/" element={
+                <div>
+                    <div className="search-main-container">
+                        <div className="search-container">
+                            <input
+                                className="search-input"
+                                type="text"
+                                value={query}
+                                onChange={e => setQuery(e.target.value)}
+                                placeholder="Search for recipes..."
+                            />
+                            <button className="search-button" onClick={handleSearch}>Search</button>
+                        </div>
+                        <div className="filter-icons">
+                            <img
+                                src="/icons/gluten-free.png"
+                                alt="Gluten Free"
+                                className="icon-button"
+                                onClick={() => handleKeywordSearch('gluten free')}
+                            />
+                            <img
+                                src="/icons/vegetarian.png"
+                                alt="Vegetarian"
+                                className="icon-button"
+                                onClick={() => handleKeywordSearch('vegetarian')}
+                            />
+                            <img
+                                src="/icons/vegan.png"
+                                alt="Vegan"
+                                className="icon-button"
+                                onClick={() => handleKeywordSearch('vegan')}
+                            />
+                        </div>
+                    </div>
+                    <div className="cards-container">
+                        <Slider {...sliderSettings}>
+                            {recipes.map((recipe: any, index: number) => (
+                                <div className="card" style={{ width: "18rem" }} key={index}>
+                                    <img src={recipe.image} className="card-img-top" alt={recipe.title} />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{recipe.title}</h5>
+                                        <p className="card-text">
+                                            <span dangerouslySetInnerHTML={{
+                                                __html: recipe.summary.length > 100
+                                                    ? recipe.summary.substring(0, 100) + '...'
+                                                    : recipe.summary
+                                            }}></span>
+                                        </p>
+                                        <button className="btn btn-secondary view-details-btn" onClick={() => toggleModal(recipe)}>View Details</button>
+                                        <button className="btn btn-secondary add-to-faves-btn" onClick={() => addToFavorites(recipe)}>Add to Faves</button>
+                                    </div>
+                                </div>
+                            ))}
+                        </Slider>
+                    </div>
+                    {isModalOpen && (
+                        <div className="modal">
+                            <div className={`modal-content ${showModalContent ? 'show' : ''}`}>
+                                <span className="close-button" onClick={() => { setModalOpen(false); setShowModalContent(false); }}>×</span>
+                                {modalRecipe && (
+                                    <>
+                                        <h5 className="card-title">{modalRecipe.title}</h5>
+                                        <img src={modalRecipe.image} className="card-img-top" alt={modalRecipe.title} />
+                                        <p dangerouslySetInnerHTML={{ __html: modalRecipe.summary }}></p>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                    )}
+                </div>
+            } />
+            <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+    </Router>
 );
-};
+          }
+
 
 export default App;
